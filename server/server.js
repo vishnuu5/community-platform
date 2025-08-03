46d5c69c45d5db2e6e2473035e984df2edbd7fec
vishnuu5 <vishnuathmakuru20@gmail.com>
2025-08-03 15:09:05 +0530

    cors update

--- server/server.js
+++ server/server.js
@@ -12,7 +12,12 @@ connectDB();
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
